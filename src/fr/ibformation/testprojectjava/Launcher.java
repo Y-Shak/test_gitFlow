ab22019b76cb97a649657665c23d69acd31ec94e
DESKTOP-5L7N2A8\ib <yousra.shaki95@gmail.com>
2021-04-09 16:08:45 +0200

    --- Adding all the project for commit

---
+++ src/fr/ibformation/testprojectjava/Launcher.java
@@ -0,0 +1,10 @@
+package fr.ibformation.testprojectjava;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+
+		System.out.println("this is a test prject for gitFlow");
+	}
+
+}
